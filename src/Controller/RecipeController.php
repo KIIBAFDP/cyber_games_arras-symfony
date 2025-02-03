bdaa6f0f4190e313841818510f402bfcb1e574a1
Mathieu Mistretta <mathieu.mistretta@gmail.com>
2025-02-03 10:12:17 +0100

    recipe update

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,25 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findAll();
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9-]+', 'id' => '\d+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(string $slug, int $id, RecipeRepository $repository): Response
     {
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', [
+                'id' => $recipe->getId(),
+                'slug' => $recipe->getSlug(),
+            ], 301);
+        }
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
+            'recipe' => $recipe,
         ]);
     }
 }
